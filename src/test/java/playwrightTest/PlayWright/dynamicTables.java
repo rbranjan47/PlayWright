b29999ad3a36c555d6a24060cec7c8121d6d5374
Rabi Ranjan Kumar <rbranjan47@gmail.com>
2026-01-04 22:58:09 +0530

    addUsersTestSteps

--- src/test/java/playwrightTest/PlayWright/dynamicTables.java
+++ src/test/java/playwrightTest/PlayWright/dynamicTables.java
@@ -14,43 +14,43 @@ import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.ElementState;
 
 public class dynamicTables {
-	@Test
-	public void dynamicTableTest() throws Exception {
-		try (Playwright playwright = Playwright.create()) {
-			Browser browser = playwright.chromium()
-					.launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("msedge"));
+    @Test
+    public void dynamicTableTest() throws Exception {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium()
+                    .launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("msedge"));
 
-			String filePath = System.getProperty("user.dir") + "\\JsonFile\\login.json";
-			BrowserContext context = browser
-					.newContext(new Browser.NewContextOptions().setStorageStatePath(Paths.get(filePath)));
+            String filePath = System.getProperty("user.dir") + "\\JsonFile\\login.json";
+            BrowserContext context = browser
+                    .newContext(new Browser.NewContextOptions().setStorageStatePath(Paths.get(filePath)));
 
-			Page page = context.newPage();
-			page.navigate("https://qa.myresman.com");
+            Page page = context.newPage();
+            page.navigate("https://qa.myresman.com");
 
-			Locator closeAdvisorLocator = page.locator("#CloseAdvisor");
-			if (closeAdvisorLocator.isEnabled()) {
-				closeAdvisorLocator.waitFor();
-				closeAdvisorLocator.click();
-			}
+            Locator closeAdvisorLocator = page.locator("#CloseAdvisor");
+            if (closeAdvisorLocator.isEnabled()) {
+                closeAdvisorLocator.waitFor();
+                closeAdvisorLocator.click();
+            }
 
-			page.click("xpath=//li[@menuname='Admin']");
+            page.click("xpath=//li[@menuname='Admin']");
 
-			page.locator("a:has-text('Properties')").click();
+            page.locator("a:has-text('Properties')").click();
 
-			ElementHandle handle = page.querySelector("div[id='Loading']");
-			handle.waitForElementState(ElementState.VISIBLE);
-			handle.waitForElementState(ElementState.HIDDEN);
+            ElementHandle handle = page.querySelector("div[id='Loading']");
+            handle.waitForElementState(ElementState.VISIBLE);
+            handle.waitForElementState(ElementState.HIDDEN);
 
-			// handling dynamic tables
-			Locator propertiesRows = page.locator("div#PropertyIndexList >> table >> tbody >> tr");
-			for (int i = 0; i < propertiesRows.count(); i++) {
-				System.out.println(propertiesRows.nth(i).innerText());
-			}
-			Thread.sleep(5000);
-			browser.close();
-			playwright.close();
-		} catch (Exception e) {
-			throw e;
-		}
-	}
+            // handling dynamic tables
+            Locator propertiesRows = page.locator("div#PropertyIndexList >> table >> tbody >> tr");
+            for (int i = 0; i < propertiesRows.count(); i++) {
+                System.out.println(propertiesRows.nth(i).innerText());
+            }
+            Thread.sleep(5000);
+            browser.close();
+            playwright.close();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
